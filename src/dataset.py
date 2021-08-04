bd5045e7035f29c135539750543ff88c747a5960
danielz02 <31590926+danielz02@users.noreply.github.com>
2021-08-03 23:16:50 -0500

    Fix crop ROI in HDF5 wrapper

--- src/dataset.py
+++ src/dataset.py
@@ -43,21 +43,24 @@ class NeonDataset:
         self.wavelength_units = self.meta['Spectral_Data']['Wavelength'].attrs['Units']
 
         self.raster_meta = {
-            "height": self.h,
-            "width": self.w,
             "crs": self.crs,
+            "nodata": -9999,
+            "width": self.w,
+            "height": self.h,
+            "dtype": np.int16,
+            "driver": "GTiff",
             "transform": self.transform
         }
 
         # TODO: More metadata fields under ["Reflectance"]["Metadata"]
 
-    def crop_roi(self, shapefile: str, dest: str):
+    def crop_roi(self, shapefile: str, dest_dir: str):
         with fiona.open(shapefile) as shp:
             trans = Transformer.from_crs(shp.crs["init"], self.crs) \
                 if shp.crs["init"].split(":")[-1] != str(self.crs.to_epsg()) else lambda x, y: (x, y)
             for feature in shp:
-                polygon, *_ = feature["coordinates"]
-                feature["coordinates"] = [[trans(*x) for x in polygon]]
+                polygon, *_ = feature["geometry"]["coordinates"]
+                feature["geometry"]["coordinates"] = [[trans.transform(*reversed(x)) for x in polygon]]
                 feat_bounds = bounds(feature)
                 if disjoint_bounds(feat_bounds, self.bounds):
                     continue
@@ -69,18 +72,25 @@ class NeonDataset:
                     int(feat_window.col_off):int(feat_window.col_off + feat_window.width),
                     :
                 ]
-                feat_meta = self.meta.copy()
+                feat_meta = self.raster_meta.copy()
                 feat_meta["height"], feat_meta["width"], feat_meta["count"] = feat_arr.shape
                 feat_meta["transform"] = feat_transform
-                with rasterio.open(
-                    os.path.join(dest, "{}.tif".format(feature["properties"]["plotID"])), "w", **feat_meta
-                ) as dest:
-                    dest.write(self.data[()].transpose(2, 0, 1))
+                dest_path = os.path.join(
+                    dest_dir, "{}_{}.tif".format(
+                        feature["properties"]["plotID"], os.path.basename(self.path).strip(".h5").strip("NEON_")
+                    )
+                )
+                with rasterio.open(dest_path, "w", **feat_meta) as dest:
+                    dest.write(feat_arr.transpose(2, 0, 1))
                     for i, wls in enumerate(self.wavelengths):
-                        dest.set_band_description(wls, i)
+                        dest.set_band_description(i + 1, "{} {}".format(wls, self.wavelength_units))
 
-    def to_tiff(self):
-        pass
+    def to_tiff(self, dest_path: str = None):
+        dest_path = self.path.replace(".h5", ".tif") if not dest_path else dest_path
+        with rasterio.open(dest_path, "w", **self.raster_meta) as dest:
+            dest.write(self.data[()].transpose(2, 0, 1))
+            for i, wls in enumerate(self.wavelengths):
+                dest.set_band_description(i + 1, "{} {}".format(wls, self.wavelength_units))
 
     def __del__(self):
         self.h5.close()
