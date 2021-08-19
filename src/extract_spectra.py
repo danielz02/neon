792aa01630d4897d6d5fd503ad4580b72a1fe5b9
danielz02 <31590926+danielz02@users.noreply.github.com>
2021-08-18 23:52:36 -0500

    Control site name in the fuzzy search for files

--- src/extract_spectra.py
+++ src/extract_spectra.py
@@ -4,7 +4,7 @@ import pandas as pd
 from glob import glob
 from pandas.tseries.offsets import DateOffset
 
-df = pd.read_csv("/scratch/sciteam/ILL_bbdv/NEON/data/NEON.DP1.10026.001.cfc_elements.expanded.csv")
+df = pd.read_csv("/scratch/sciteam/ILL_bbdv/NEON/data/NEON.DP1.10026.001.cfc_carbonNitrogen.expanded.csv")
 df["collectDate"] = pd.to_datetime(df["collectDate"])
 
 rows = []
@@ -12,13 +12,23 @@ for i in range(len(df)):
     row = df.loc[i]
     yymm = row["collectDate"].strftime("%Y-%m")
     fid = str(row["FID"]).zfill(4)
-    p = sorted(glob("/scratch/sciteam/ILL_bbdv/NEON/DP3.30006.001/*/{yymm}/{fid}_*.tif".format(yymm=yymm, fid=fid)))
+    site_id = row["siteID"]
+    p = sorted(glob(
+        "/scratch/sciteam/ILL_bbdv/NEON/DP3.30006.001/{site}/{yymm}/{fid}_*.tif"
+        .format(site=site_id, yymm=yymm, fid=fid)
+    ))
     if len(p) == 0:
         yymm = (row["collectDate"] + DateOffset(20)).strftime("%Y-%m")
-        p = sorted(glob("/scratch/sciteam/ILL_bbdv/NEON/DP3.30006.001/*/{yymm}/{fid}_*.tif".format(yymm=yymm, fid=fid)))
+        p = sorted(glob(
+            "/scratch/sciteam/ILL_bbdv/NEON/DP3.30006.001/{site}/{yymm}/{fid}_*.tif"
+            .format(site=site_id, yymm=yymm, fid=fid)
+        ))
     if len(p) == 0:
         yymm = (row["collectDate"] - DateOffset(20)).strftime("%Y-%m")
-        p = sorted(glob("/scratch/sciteam/ILL_bbdv/NEON/DP3.30006.001/*/{yymm}/{fid}_*.tif".format(yymm=yymm, fid=fid)))
+        p = sorted(glob(
+            "/scratch/sciteam/ILL_bbdv/NEON/DP3.30006.001/{site}/{yymm}/{fid}_*.tif"
+            .format(site=site_id, yymm=yymm, fid=fid)
+        ))
     if len(p) == 0:
         print("Not Found:", fid, row["plotID"], row["collectDate"].strftime("%Y-%m-%d"))
     else:
@@ -32,12 +42,12 @@ for i in range(len(df)):
             arrs.append(arr)
         arrs = np.concatenate(arrs, axis=0)
         df_plot = pd.concat([
-            pd.DataFrame({"UID": [row["uid"]] * len(arrs), "FID": [row["FID"]] * len(arrs), "Date": [yymm] * len(arr)}),
+            pd.DataFrame({"UID": [row["UID"]] * len(arrs), "FID": [row["FID"]] * len(arrs), "Date": [yymm] * len(arr)}),
             pd.DataFrame(arrs, columns=range(c)).reset_index(drop=True)
         ], axis=1).reindex()
         rows.append(df_plot)
 rows = pd.concat(rows, axis=0)
 rows.to_csv(
-    "/scratch/sciteam/ILL_bbdv/NEON/data/NEON.DP1.10026.001.cfc_elements.expanded.spectra.csv",
+    "/scratch/sciteam/ILL_bbdv/NEON/data/NEON.DP1.10026.001.cfc_carbonNitrogen.expanded.spectra.csv",
     index=False
 )
