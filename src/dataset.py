964e2195488f3941b3c9a5e2c42b79455235a858
danielz02 <31590926+danielz02@users.noreply.github.com>
2021-10-26 23:59:16 -0500

    Add SpectraDataset class to wrap tabular csv dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,9 +1,11 @@
 import os
 import zlib
+import pandas as pd
 import h5py
 import fiona
 import rasterio
 import numpy as np
+import torch
 from tqdm import tqdm
 from affine import Affine
 from osgeo import ogr, osr
@@ -12,6 +14,8 @@ from collections import namedtuple
 from rasterio.features import bounds
 from rasterio.coords import disjoint_bounds
 from rasterio.windows import from_bounds, transform, intersection
+from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset, random_split, Dataset
 
 Bounds = namedtuple("Bounds", ["left", "bottom", "right", "top"])
 
@@ -124,3 +128,93 @@ class NeonDataset:
 
     def __del__(self):
         self.h5.close()
+
+
+class SpectralDataset(Dataset):
+    def __init__(self, path: str, seq_len: int, target_name: str, scale: float = None, transforms=None, position_encode=False):
+        super().__init__()
+        self.scale = scale
+        self.seq_len = seq_len
+        self.df = pd.read_csv(path)
+        self.transforms = transforms
+        self.target_name = target_name
+        self.position_encode = position_encode
+        try:
+            self.wls = torch.tensor(self.df.columns[:seq_len].astype(dtype=np.float32), dtype=torch.float32)
+        except:
+            print("")
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        x_tensor = torch.from_numpy(self.df.iloc[idx, :self.seq_len].to_numpy().astype(np.float32))
+        y_tensor = torch.tensor(self.df[self.target_name].iloc[idx].astype(np.float32))
+        if self.transforms:
+            x_tensor = self.transforms(x_tensor)
+        if self.scale:
+            x_tensor *= self.scale
+        if self.position_encode:
+            x_tensor = torch.vstack([x_tensor, self.wls]).T
+        else:
+            x_tensor = x_tensor.unsqueeze(dim=1)
+        return x_tensor, y_tensor
+
+
+class GaussianNoise(object):
+    def __init__(self, std=0.01):
+        self.std = std
+
+    def __call__(self, batch):
+        if not isinstance(batch, torch.Tensor):
+            batch = torch.tensor(batch)
+        return batch + torch.normal(0, self.std, size=batch.shape)
+
+
+def read_dataset(
+    path: str, target_name: str, seq_len, split_ratio: float = None, scale: float = None,
+        stratify: bool = False
+):
+    """
+    Read any tabular dataset (with the last column being labels) as PyTorch TensorDataset
+    :param stratify: whether to stratify the dataset while splitting
+    :param scale: scale factor for the spectra
+    :param seq_len: the length of the input features
+    :param target_name: the name of the label column in the CSV
+    :param path: path to the csv file
+    :param split_ratio: Train/Validation split ratio
+    :return: Train and validation dataset if split_ratio is define, the entire dataset otherwise
+    """
+    if split_ratio:
+        assert split_ratio >= 0
+        assert split_ratio <= 1
+
+    df = pd.read_csv(path)
+    x_arr = df.iloc[:, :seq_len]
+    y_arr = df[target_name]
+
+    x_tensor = torch.tensor(x_arr.to_numpy(), dtype=torch.float32)
+    if scale:
+        x_tensor *= scale
+    x_tensor = x_tensor.unsqueeze(1)  # Add channel dimension -> (N, C, seq_len)
+    y_tensor = torch.tensor(y_arr, dtype=torch.float32)
+    dataset = TensorDataset(x_tensor, y_tensor)
+
+    if split_ratio:
+        train_size = int(len(dataset) * split_ratio)
+        val_size = len(dataset) - train_size
+        if stratify:
+            bin_edges = np.histogram_bin_edges(y_tensor, bins="auto")
+            print(bin_edges)
+            y_binned = np.digitize(y_tensor, bin_edges, right=True)
+            train_set, val_set = train_test_split(
+                dataset, train_size=train_size, test_size=val_size, random_state=0,
+                shuffle=True, stratify=y_binned
+            )
+        else:
+            train_set, val_set = random_split(
+                dataset=dataset, lengths=[train_size, val_size], generator=torch.manual_seed(0)
+            )
+        return train_set, val_set
+    else:
+        return dataset
